5cbcc1052a56327817f59ade8f01854a629c7047
slowrunr <mungaslov.nft@gmail.com>
2023-09-05 17:02:16 +0900

    prettier revision of your code. pls acpt plreqst

--- popup.js
+++ popup.js
@@ -1,24 +1,26 @@
-const POPUP_OPENED_CLASSNAME = 'service__popup_open';
-const BODY_FIXED_CLASSNAME = 'body_fixed';
+const POPUP_OPENED_CLASSNAME = "service__popup_open";
+const BODY_FIXED_CLASSNAME = "body_fixed";
 
-const bodyNode = document.querySelector('body');
-const popupNode = document.querySelector('.js-popup');
-const btnOpenNode = document.querySelector('.js-btn');
-const popupContentNode = document.querySelector('.js-popup__content')
-const btnCloseNode = document.querySelector('.js-popup__close-btn');
+const bodyNode = document.querySelector("body");
+const popupNode = document.querySelector(".js-popup");
+const btnOpenNode = document.querySelector(".js-btn");
+const popupContentNode = document.querySelector(".js-popup__content");
+const btnCloseNode = document.querySelector(".js-popup__close-btn");
 
-btnOpenNode.addEventListener('click', togglePopup);
-btnCloseNode.addEventListener('click', togglePopup);
+btnOpenNode.addEventListener("click", togglePopup);
+btnCloseNode.addEventListener("click", togglePopup);
 
-popupNode.addEventListener('click', (event) => {
-    const isClickOutsideContent = !event.composedPath().includes(popupContentNode)
+popupNode.addEventListener("click", (event) => {
+  const isClickOutsideContent = !event
+    .composedPath()
+    .includes(popupContentNode);
 
-    if (isClickOutsideContent) {
-        togglePopup();
-    }
-})
+  if (isClickOutsideContent) {
+    togglePopup();
+  }
+});
 
 function togglePopup() {
-    popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
-    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+  popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
+  bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
 }
